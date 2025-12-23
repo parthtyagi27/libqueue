a439d45b78f9b990198d2e2400d6a11380c4d637
Parth Tyagi <parth.27@outlook.com>
2025-12-22 21:27:48 -0600

    added more ring buffer tests

--- tests/ring_buffer_test.cpp
+++ tests/ring_buffer_test.cpp
@@ -3,10 +3,23 @@
 #include <optional>
 #include "../src/containers/ringbuffer.hpp"
 
+void test_empty_queue() {
+    queue_containers::RingBuffer<int, 100> queue{};
+    queue.pop();
+}
 
-int main() {
+void test_pop_before_push() {
+    queue_containers::RingBuffer<int, 100> queue{};
+    queue.pop();
+    queue.push(100);
+    std::cout << queue.size() << "\n";
+    std::cout << *queue.front() << "\n";
+    std::cout << queue.size() << "\n";
+}
+
+void test_push_pop() {
     queue_containers::RingBuffer<int, 5> buffer{};
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 4; i++) {
         bool val = buffer.push(i);
         if (val)
             std::cout << "success\n";
@@ -22,6 +35,19 @@ int main() {
         buffer.pop();
     }
     std::cout << "\n";
+    buffer.push(7);
+    std::cout << buffer.get_write_idx() << "\n";
+    std::cout << buffer.get_read_idx() << "\n";
 
+    std::cout << "\n";
+    buffer.push(8);
+    std::cout << buffer.get_write_idx() << "\n";
+    std::cout << buffer.get_read_idx() << "\n";
+}
+
+int main() {
+    test_empty_queue();
+    test_push_pop();
+    test_pop_before_push();
     return 0;
 }
