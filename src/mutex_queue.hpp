8ca8535e7e38229b699c48f14fe103385d42992e
Parth Tyagi <parth.27@outlook.com>
2025-12-23 18:37:07 -0600

    init commit for thread safe non blocking queue

--- src/mutex_queue.hpp
+++ src/mutex_queue.hpp
@@ -1,6 +1,8 @@
 #pragma once
-#include <array>
+#include "options.hpp"
+#include <condition_variable>
 #include <cstddef>
+#include <iostream>
 #include <mutex>
 #include <optional>
 
@@ -19,23 +21,36 @@ namespace queues {
  * It supports 2 operations, front() and push(T)
  */
 
-template <typename T, class Container>
+template <typename T, class Container, options::BlockingOptions blocking_option>
 class GlobalMutexQueue {
 private:
     Container queue;
     size_t num_elements;
     std::mutex mtx;
+    std::condition_variable cv;
 
 public:
     // delete all copy and move contstructors, not sure what it means to "copy" or "move" a thread-safe queue
-    GlobalMutexQueue() : queue{}, num_elements{0}, mtx{} {};
+    GlobalMutexQueue() : queue{}, num_elements{0}, mtx{}, cv{} {
+        static_assert(blocking_option == options::BlockingOptions::NonBlocking, "Non blocking not supported yet\n");
+    }
     GlobalMutexQueue(const GlobalMutexQueue&) = delete;
     GlobalMutexQueue(GlobalMutexQueue&&) = delete;
     GlobalMutexQueue& operator=(const GlobalMutexQueue&) = delete;
     GlobalMutexQueue& operator=(GlobalMutexQueue&&) = delete;
 
-    std::optional<T> front();
-    bool push(const T& object);
+    std::optional<T> front() {
+        std::lock_guard<std::mutex> lock{mtx};
+        return queue.front();
+    }
+    void push(const T& object) {
+        std::lock_guard<std::mutex> lock{mtx};
+        queue.push(object);
+    }
+    void pop() {
+        std::lock_guard<std::mutex> lock{mtx};
+        queue.pop();
+    }
 };
 
 
